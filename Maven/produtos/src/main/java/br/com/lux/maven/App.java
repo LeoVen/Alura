8487139b2116fd1e2694b1639f426c86ffa81e69
LeoVen <lvenk26@gmail.com>
2018-02-21 17:27:05 -0300

    v-18-02.21.w

---
+++ Maven/produtos/src/main/java/br/com/lux/maven/App.java
@@ -0,0 +1,13 @@
+package br.com.lux.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
